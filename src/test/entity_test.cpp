c3f5cc02ccc9ccc9ae8d574e84dcb795d6092978
sthomasen7 <sthomasen7@gmail.com>
2025-12-23 23:55:12 -0600

    Test entity types

--- src/test/entity_test.cpp
+++ src/test/entity_test.cpp
@@ -64,7 +64,6 @@ MAKE_TEST(PositionComponentTest){
 
     idx+=1;
   }
-
 }
 
 MAKE_TEST(ViewListTest){
@@ -98,9 +97,101 @@ MAKE_TEST(ViewListTest){
   CHECK_TRUE(eview == nullptr);
 }
 
+
+MAKE_TEST(EntityTypesTest){
+  TEST_START();
+
+  EntityManager entity_manager;
+  entity_manager.clear();
+  Entity entity_position = entity_manager.createEntity();
+  Entity entity_transform = entity_manager.createEntity();
+  Entity entity_shader = entity_manager.createEntity();
+  Entity entity_mesh = entity_manager.createEntity();
+  Entity entity_light = entity_manager.createEntity();
+  Entity entity_light_angle = entity_manager.createEntity();
+  Entity entity_light_direction = entity_manager.createEntity();
+  Entity entity_camera = entity_manager.createEntity();
+  Entity entity_render = entity_manager.createEntity();
+
+  entity_manager.addPositionComponent(entity_position, {});
+  entity_manager.addTransformComponent(entity_transform, {});
+  entity_manager.addShaderComponent(entity_shader, {});
+  entity_manager.addMeshComponent(entity_mesh, {});
+  entity_manager.addLightComponent(entity_light, {});
+  entity_manager.addLightDirectionComponent(entity_light_direction, {});
+  entity_manager.addLightAngleComponent(entity_light_angle, {});
+  entity_manager.addCameraComponent(entity_camera, {});
+  entity_manager.addRenderComponent(entity_render, {});
+
+  try{
+    entity_manager.getPosition(entity_position);
+  } catch(...){
+    LOG << "Failed to get position entity." << std::endl;
+    CHECK_TRUE(false);
+  }
+
+  try{
+    entity_manager.getTransform(entity_transform);
+  } catch(...){
+    LOG << "Failed to get transform entity." << std::endl;
+    CHECK_TRUE(false);
+  }
+
+  try{
+    entity_manager.getShader(entity_shader);
+  } catch(...){
+    LOG << "Failed to get shader entity." << std::endl;
+    CHECK_TRUE(false);
+  }
+
+  try{
+    entity_manager.getMesh(entity_mesh);
+  } catch(...){
+    LOG << "Failed to get mesh entity." << std::endl;
+    CHECK_TRUE(false);
+  }
+
+  try{
+    entity_manager.getLight(entity_light);
+  } catch(...){
+    LOG << "Failed to get light entity." << std::endl;
+    CHECK_TRUE(false);
+  }
+
+  try{
+    entity_manager.getLightAngle(entity_light_angle);
+  } catch(...){
+    LOG << "Failed to get light angle entity." << std::endl;
+    CHECK_TRUE(false);
+  }
+
+  try{
+    entity_manager.getLightDirection(entity_light_direction);
+  } catch(...){
+    LOG << "Failed to get light direction entity." << std::endl;
+    CHECK_TRUE(false);
+  }
+
+  try{
+    entity_manager.getCamera(entity_camera);
+  } catch(...){
+    LOG << "Failed to get camera entity." << std::endl;
+    CHECK_TRUE(false);
+  }
+
+  try{
+    entity_manager.getRender(entity_render);
+  } catch(...){
+    LOG << "Failed to get render entity." << std::endl;
+    CHECK_TRUE(false);
+  }
+
+}
+
 void set_entity_tests(){
   TEST(EntityTestSuite, InitTest);
   TEST(EntityManagerSuite, BasicIterationTest);
   TEST(EntityManagerSuite, PositionComponentTest);
   TEST(EntityManagerSuite, ViewListTest);
+  TEST(EntityManagerSuite, EntityTypesTest);
 }
